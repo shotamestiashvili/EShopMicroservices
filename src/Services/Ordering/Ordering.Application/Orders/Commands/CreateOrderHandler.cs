2b27664cf18a724932058e1111af5988ecd797f4
shmestiashvili <shmestiashvili@upgaming.com>
2025-03-05 18:54:27 +0400

    app layer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands;
+
+public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        // create order
+        //save to db
+        //return resutl
+        
+        throw new NotImplementedException();
+    }
+}
